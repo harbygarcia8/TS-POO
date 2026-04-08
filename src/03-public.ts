c52683bd50125edce0f37a6fcf8346d9f4c71647
Harby Garcia <harby-96@hotmail.com>
2026-04-08 10:12:25 -0500

    add MyDate class with methods for formatting and date manipulation

---
+++ src/03-public.ts
@@ -0,0 +1,32 @@
+class MyDate {
+    public year: number;
+    public month: number;
+    public day: number;
+
+    constructor(year: number, month: number, day:number) {
+        this.year = year,
+        this.month = month,
+        this.day = day
+    }
+    public printFormat(): string {
+        return `${this.day}/${this.month}/${this.year}`;
+    }
+
+    public add(amount: number, type: 'days' | 'months' | 'years') {
+        if (type === 'days') {
+            this.day += amount;
+        } 
+        if (type === 'months') {
+            this.month += amount;
+        } 
+        if (type === 'years') {
+            this.year += amount;
+        }
+    }
+}
+
+
+const myDate = new MyDate(2026,3,12)
+console.log(myDate.printFormat())
+myDate.add(3,'days')
+console.log(myDate.printFormat())
