d9584344f2de9127c158a4acbd6e5a807c106d12
Harby Garcia <harby-96@hotmail.com>
2026-04-08 12:11:55 -0500

    add MyDate class with methods for date manipulation and formatting

---
+++ src/04-private.ts
@@ -0,0 +1,46 @@
+class MyDate {
+    private year: number;
+    private month: number;
+    private day: number;
+
+    constructor(year: number, month: number, day:number) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    
+    private addPadding(value: number): string {
+        if (value < 10) {
+            return `0${value}`;
+        }
+        return `${value}`;
+    }
+    
+    public printFormat(): string {
+        const day = this.addPadding(this.day)
+        const month = this.addPadding(this.month)
+        return `${day}/${month}/${this.year}`;
+    }
+
+    public add(amount: number, type: 'days' | 'months' | 'years') {
+        if (type === 'days') {
+            this.day += amount;
+        } 
+        if (type === 'months') {
+            this.month += amount;
+        } 
+        if (type === 'years') {
+            this.year += amount;
+        }
+    }
+}
+
+
+const myDate = new MyDate(2026,3,10)
+console.log(myDate.printFormat())
+myDate.add(3,'days')
+
+// This dont work because day is private
+//myDate.day = 12
+//console.log(myDate.day)
+console.log(myDate.printFormat())
