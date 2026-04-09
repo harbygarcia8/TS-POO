4fd8020e965948ea8d293d9db492ca40880ce4d2
Harby Garcia <harby-96@hotmail.com>
2026-04-09 15:47:16 -0500

    add: implement Animal and Dog classes with methods for movement and greeting

---
+++ src/09-protected.ts
@@ -0,0 +1,54 @@
+export class Animal {
+
+    constructor(protected name: string) {
+    }
+
+    move() {
+        console.log('Moving along!')
+    }
+
+    greeting(){
+        return `Hello, I'm ${this.name}`
+    }
+
+    protected doSomething() {
+        console.log('doooo')
+    }
+
+}
+
+
+export class Dog extends Animal {
+
+    constructor(name: string, readonly _owner: string) {
+        super(name)
+    }
+
+    woof(value:number): void {
+        for (let i=0; i<value; i++) {
+            console.log(`woof, soy ${this.name}`)
+        }
+        this.doSomething()
+    }
+
+    get owner() {
+        return this._owner
+    }
+
+    move(): void {
+        console.log('moving as a dog')
+        super.move()
+    }
+    
+}
+
+const spike = new Dog('Spike', 'Harby')
+spike.move()
+console.log(spike.greeting())
+spike.woof(4)
+spike.move()
+
+console.log(spike.owner)
+spike.move()
+
+
