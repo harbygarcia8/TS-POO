11602ee724274947d7929bb9797942d3e772052f
Harby Garcia <harby-96@hotmail.com>
2026-04-10 13:51:10 -0500

    refactor: clean up MyDate class constructor and improve comments

--- src/01-class.ts
+++ src/01-class.ts
@@ -1,11 +1,19 @@
+// La creación de clases en TypeScript es similar a la de JavaScript, 
+// pero con la adición de tipos para las propiedades y parámetros del 
+// constructor. Aquí hay un ejemplo de cómo crear una clase `MyDate` 
+// que representa una fecha:
+
+// Yendo a los coneptos de programación orientada a objetos, 
+// una clase es un molde para crear objetos.
+
 export class MyDate {
     year: number;
     month: number;
     day: number;
 
     constructor(year: number, month: number, day:number) {
-        this.year = year,
-        this.month = month,
+        this.year = year
+        this.month = month
         this.day = day
 
     }
