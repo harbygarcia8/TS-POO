9975af633e5e80b89e29526418513bc85678bd7f
Harby Garcia <harby-96@hotmail.com>
2026-04-12 11:49:17 -0500

    docs: improve comments and structure in 12-abstract.ts for better clarity on abstract classes and methods

--- src/12-abstract.ts
+++ src/12-abstract.ts
@@ -1,12 +1,48 @@
 import {Animal, Dog} from './09-protected'
 
 // Ya no puedo crear instancias del objeto padre, 
-// porque al dejar mencionar que este es una clase abstracta, 
+// porque al mencionar que esta es una clase abstracta, 
 // la creación de instancias deben ser a partir de clases hijas.
  
 //❌ const spike = new Animal('Spike')
 // spike.greeting()
 
+//Ejemplo
+export class Cat extends Animal {
+    constructor(name:string, private _owner: string) {
+        super(name)
+    }
+
+    //Existen metodos abstractos, son metodos que no tienen 
+    //implementación, solo definen la firma del método,
+    //y las clases hijas deben implementar ese método.
+    //Y este tipo de metodos solo pueden ser definidos en clases
+    // abstractas
+    // abstract jumb(): string
+
+    climb(): string {
+        return ``
+    }
+
+    get owner() {
+        return this._owner
+    }
+
+    set owner(value: string) {
+        this._owner = value
+    }
+}
+
+
+
+
 const viole = new Dog('Violeta', 'Maria')
 viole.greeting()
 viole.woof(3)
+
+const tommy = new Cat('Tommy', 'Harby')
+
+tommy.owner = 'jlkrjg'
+
+console.log(tommy.greeting())
+console.log(tommy.owner)
